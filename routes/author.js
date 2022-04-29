83a0ad34a2c0332e9385c7b9bde08c72c9079267
Árpád <developedbybarley@gmail.com>
2022-04-29 11:40:07 +0200

    Author route is added, tested!

---
+++ routes/author.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const Author = require('../models/author');
+
+router.get('/', (req, res) => {
+  res.render('author/index')
+})
+
+router.get('/new', (req, res) => {
+  const author = new Author({});
+  res.render('author/new', { author: author });
+})
+
+router.post('/', bodyParser.json(), async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+
+  try {
+    const newAuthor = await author.save();
+    res.redirect('/authors')
+    console.log(newAuthor + ' : Author is saved!')
+  } catch (error) {
+    console.log(error)
+    res.render('author/new', {author: author, errorMessage: 'Can not save author'})
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,37 @@
+/** Express and libraries */
 const express = require('express')
 const app = express()
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+//Set routers
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
+
+
+/** Server  */
+const mongoose = require('mongoose');
+
+app.set('view engine', 'ejs') ;
+app.set('views', __dirname  + '/views');
+app.set('layout', 'layouts/layout');
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//Routes
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
+
+
+// Mongoose database connection!
+mongoose.connect('mongodb://localhost/myBraryPcDb');
+const db = mongoose.connection
+.on('error', error => console.log(error))
+.once('open', () => console.log('Connected to Mongoose!'))
 
 app.listen(8080)
